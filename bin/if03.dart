d9bea95ebaa9b6f44e36c73e6e2ba22f031603aa
Mirjalol <jalolubaydullayev575@gmail.com>
2023-03-02 12:36:49 +0500

    if03

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,21 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
+    int func(int a,int b, int c) {
+     int answer = 0;
+     if (a < b && a < b){
+        return answer + b;
+     }else{
+        if (b < a && b < a) {
+            return answer + a;
+        }else{
+            if (c < a && c < a) {
+                return answer + c;
+            }
+        }
+        return answer;
+     }
+    }
 void main() {
+    print(func(1, 4, 2));
 }
